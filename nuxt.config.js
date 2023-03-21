52f0d157c0a60749213dc46f4f2e012fca62abdd
vagkalosynakis <vagkalosynakis@gmail.com>
2023-03-21 17:35:34 +0200

    added primevue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  css: [
+    'primevue/resources/themes/saga-blue/theme.css',
+    'primevue/resources/primevue.css',
+    'primeicons/primeicons.css',
+  ],
+
   modules: [
     '@pinia/nuxt',
   ],
+
+  build: {
+    transpile: [
+      'primevue'
+    ]
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-         {{ name }}
+         {{ date }}
     </div>
+    <Calendar v-model="date" showTime/>
 </template>
 
 <script setup>
 import { useGlobalStore } from "@/store/globalStore";
+import Calendar from 'primevue/calendar';
 
 const store = useGlobalStore()
-
 const name = store.fullName
+const date = ref(null)
 </script>
